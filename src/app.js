af7ffb2f23e3c5490a1700a77ae2846013f8610f
Fabian <thrasherxd16@gmail.com>
2023-06-28 03:01:21 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,8 +33,6 @@ app.get('/eventos', async (req, res) => {
         const eventDate = moment(evento.event_date, 'YYYY-MM-DD'); 
         const today = moment();
         const differenceInDays = eventDate.diff(today, 'days');
-        console.log(differenceInDays);
-        console.log(evento);
   
         if (differenceInDays >= 0 && differenceInDays <= 1) {
           await sendEventNotification(evento);
